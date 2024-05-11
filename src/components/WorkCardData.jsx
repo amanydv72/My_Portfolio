60dafd3bbd87e45ff26aca1d1b5d6b7b1dc995f6
Aman Yadav <amanydv879587@gmail.com>
2024-05-11 20:06:48 +0530

    add github link of projects

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -8,21 +8,21 @@ const ProjectCardData = [
     title: "Yoytube clone",
     text: " YouTube clone project created using ReactJS that replicates some of the core features and functionalities of the YouTube platform. The project typically includes features such as search, playback, filter, and recommendations.",
     view: "https://amanydv-yt-clone.netlify.app/",
-    source: "/",
+    source: "https://github.com/amanydv72/youtube_clone.git",
   },
   {
     imgscr: Pro2,
     title: "Bitwise Calculator",
     text: "It takes two numbers as input and performs operations such as AND, OR, and XOR on their corresponding bits. It also  enables users to convert numbers from one numbering system to another(Binary, Decimal, Octal, and Hexadecimal)",
     view: "https://bitwise-calculator01.netlify.app/",
-    source: "/",
+    source: "https://github.com/amanydv72/Bitwise-Calculator.git",
   },
   {
     imgscr: Pro3,
     title: "Calender with schedule task",
     text: "A calendar with a schedule task project is a tool that allows individuals or teams to manage their time effectively by tracking their daily, weekly, or monthly activities and scheduling tasks or events on specific dates.",
     view: "https://calender-schedule-ay.netlify.app/",
-    source: "/",
+    source: "https://github.com/amanydv72/calendar-with-scheduke-task.git",
   },
 ];
 
